e2aa10ae8ed560724113b463b1dd815aa7a098db
Giovanni Martins <giovannimartins2000@gmail.com>
2022-07-13 19:50:57 -0300

    added functions to get actual quick replies and to add new quick reply

--- which-salary.js
+++ which-salary.js
@@ -1,1 +1,18 @@
-document.body.style.border = "5px solid red"
+(() => {
+    const newQuickReply = "Muito legal, mas qual o salário ?";
+
+    const getQuickReplies = () => {
+        const quickReplies = conversationsQuickReplies.querySelectorAll(".msg-s-message-list__quick-reply");
+        return quickReplies;
+    }
+
+    const addNewQuickReply = () => {
+        console.log("Adicionando nova resposta rápida...");
+
+        const quickReplies = getQuickReplies();
+        const newQuickReplyElement = document.createElement("div");
+        newQuickReplyElement.classList.add("msg-s-message-list__quick-reply");
+        newQuickReplyElement.innerHTML = newQuickReply;
+        quickReplies[0].after(newQuickReplyElement);
+    }
+})();
